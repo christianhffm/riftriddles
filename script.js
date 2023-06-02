637b488cebd074cf2926001b6bb3052b78dd5ca7
christianhffm <your_donotreply_email_id>
2023-06-02 11:21:08 +0200

    added a :featured heading

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function toggleDarkMode() {
   var body = document.body;
   var icon = document.getElementById("mode-icon");
-  var rowContainer = document.querySelector(".rowcontainer");
-  var backgroundContainer = document.querySelector(".backgroundcontainer");
+  var featureContent = document.querySelector(".featurecontent");
 
   body.classList.toggle("dark-mode");
   if (body.classList.contains("dark-mode")) {
     icon.setAttribute("src", "gfx/sun.png");
     icon.setAttribute("alt", "Light Mode");
     body.style.backgroundColor = "rgb(0, 10, 20)";
-    rowContainer.style.background = "linear-gradient(rgb(210, 210, 210), rgb(0, 10, 20))";
+    featureContent.style.background = "linear-gradient(rgb(210, 210, 210), rgb(0, 10, 20))";
     localStorage.setItem("darkMode", "true");
   } else {
     icon.setAttribute("src", "gfx/moon.png");
     icon.setAttribute("alt", "Dark Mode");
     body.style.backgroundColor = "";
-    rowContainer.style.background = "";
+    featureContent.style.background = "";
     localStorage.setItem("darkMode", "false");
   }
 }
@@ -24,20 +23,19 @@ document.addEventListener("DOMContentLoaded", function () {
   var darkMode = localStorage.getItem("darkMode");
   var body = document.body;
   var icon = document.getElementById("mode-icon");
-  var rowContainer = document.querySelector(".rowcontainer");
-  var backgroundContainer = document.querySelector(".backgroundcontainer");
+  var featureContent = document.querySelector(".featurecontent");
 
   if (darkMode === "true") {
     body.classList.add("dark-mode");
     icon.setAttribute("src", "gfx/sun.png");
     icon.setAttribute("alt", "Light Mode");
     body.style.backgroundColor = "rgb(0, 10, 20)";
-    rowContainer.style.background = "linear-gradient(rgb(210, 210, 210), rgb(0, 10, 20))";
+    featureContent.style.background = "linear-gradient(rgb(210, 210, 210), rgb(0, 10, 20))";
   } else {
     body.classList.remove("dark-mode");
     icon.setAttribute("src", "gfx/moon.png");
     icon.setAttribute("alt", "Dark Mode");
     body.style.backgroundColor = "";
-    rowContainer.style.background = "";
+    featureContent.style.background = "";
   }
 });
