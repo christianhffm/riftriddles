c0c1be7c7d1060ad7c964112c6a1f752806e7125
christianhffm <your_donotreply_email_id>
2023-07-04 12:21:34 +0200

    removed the champions_quiz, wip on skins_quiz

--- quizlogic/skins_quiz.js
+++ quizlogic/skins_quiz.js
@@ -1,72 +1,100 @@
-function initializeQuestions() {
-    fetch('../data/championFull.json')
-      .then(response => response.json())
-      .then(data => {
-        var championData = {};
-  
-        for (var championKey in data.data) {
-          if (data.data.hasOwnProperty(championKey)) {
-            var champion = data.data[championKey];
-            var championId = champion.id;
-            var skins = champion.skins;
-  
-            if (skins && Array.isArray(skins)) {
-              var skinData = skins.map(skin => {
-                return {
-                  num: skin.num,
-                  name: skin.name
-                };
-              });
-  
-              championData[championId] = {
-                championName: champion.name,
-                skins: skinData
-              };
-            }
-          }
-        }
-  
-        console.log("Champion Data:", championData);
-  
-        const randomChampionSkin = getRandomChampionSkin(championData);
-  
-        // Create the image path using champion name and skin ID
-        const imagePath = `../splashes/${randomChampionSkin.championName}_${randomChampionSkin.skinNumber}.jpg`;
-  
-        // Create an <img> element and set its source attribute
-        var skinImage = document.createElement('img');
-        skinImage.src = imagePath;
-  
-        // Insert the image element into the <div> container
-        var skinDiv = document.getElementById('skinDiv');
-        skinDiv.innerHTML = ''; // Clear previous content
-        skinDiv.appendChild(skinImage);
-  
-        console.log("Random Champion Name:", randomChampionSkin.championName);
-        console.log("Random Skin Number:", randomChampionSkin.skinNumber);
-        console.log("Random Skin Name:", randomChampionSkin.skinName);
-      })
-      .catch(error => {
-        console.log("Error fetching JSON:", error);
-      });
+// Function to fetch champion data from JSON file
+function fetchChampionData() {
+  return fetch('../data/championFull.json')
+    .then(response => response.json())
+    .then(data => Object.values(data.data))
+    .catch(error => {
+      console.log('Error:', error);
+      return [];
+    });
+}
+
+// Function to filter skins based on user input
+function filterSkins(champions, inputValue) {
+  const lowercaseInput = inputValue.toLowerCase();
+  return champions.reduce((matches, champion) => {
+    const matchingSkins = champion.skins.filter(skin =>
+      skin.name.toLowerCase().includes(lowercaseInput) && skin.name.toLowerCase() !== 'default'
+    );
+    return matches.concat(matchingSkins.map(skin => ({ ...skin, championName: champion.name })));
+  }, []);
+}
+
+
+// Function to generate the autocomplete dropdown list
+function generateDropdownList(skins) {
+  const championList = document.getElementById('champion-list');
+  championList.innerHTML = '';
+
+  skins.forEach(skin => {
+    const listItem = document.createElement('li');
+    const image = document.createElement('img');
+    image.src = `../splashes/${skin.championName}_${skin.num}.jpg`;
+
+    const skinName = skin.name.toLowerCase() === 'default' ? skin.championName : skin.name;
+    listItem.textContent = `${skinName}`;
+    listItem.insertBefore(image, listItem.firstChild);
+
+    listItem.addEventListener('click', () => {
+      const answerInput = document.getElementById('answer');
+      answerInput.value = `${skinName}`;
+      championList.innerHTML = '';
+    });
+    championList.appendChild(listItem);
+  });
+}
+
+
+
+// Function to handle the submit button click event
+function handleSubmit() {
+  const answerInput = document.getElementById('answer');
+  const userAnswer = answerInput.value.trim().toLowerCase();
+
+  const skinDiv = document.getElementById('skin');
+  const championName = skinDiv.dataset.championName.toLowerCase();
+  const skinName = skinDiv.dataset.skinName.toLowerCase();
+
+  if (userAnswer === `${skinName} (${championName})`) {
+    console.log('Correct answer!');
+  } else {
+    console.log('Incorrect answer!');
   }
+
+  // Clear the answer input
+  answerInput.value = '';
+}
+
+// Function to handle user input and trigger autocomplete
+function handleInput() {
+  const answerInput = document.getElementById('answer');
+  const inputValue = answerInput.value;
   
-  function getRandomChampionSkin(championData) {
-    const championIds = Object.keys(championData);
-    const randomChampionId = championIds[Math.floor(Math.random() * championIds.length)];
-    const champion = championData[randomChampionId];
-    const championName = champion.championName;
-    const skins = champion.skins;
-    const randomSkin = skins[Math.floor(Math.random() * skins.length)];
-    const skinNumber = randomSkin.num;
-    const skinName = randomSkin.name;
-  
-    return {
-      championName: championName,
-      skinNumber: skinNumber,
-      skinName: skinName
-    };
+  fetchChampionData()
+    .then(champions => filterSkins(champions, inputValue))
+    .then(filteredSkins => generateDropdownList(filteredSkins));
+}
+
+// Function to hide the dropdown list on blur
+function handleBlur() {
+  const championList = document.getElementById('champion-list');
+  setTimeout(() => (championList.innerHTML = ''), 0);
+}
+
+// Function to handle showing the dropdown list when clicking back into the text box
+function handleInputFocus() {
+  const answerInput = document.getElementById('answer');
+  const inputValue = answerInput.value.trim().toLowerCase();
+
+  if (inputValue) {
+    fetchChampionData()
+      .then(champions => filterSkins(champions, inputValue))
+      .then(filteredSkins => generateDropdownList(filteredSkins));
   }
-  
-  initializeQuestions();
-  
+}
+
+// Event listeners
+document.getElementById('answer').addEventListener('input', handleInput);
+document.getElementById('answer').addEventListener('blur', handleBlur);
+document.getElementById('answer').addEventListener('focus', handleInputFocus);
+document.getElementById('submit').addEventListener('click', handleSubmit);
