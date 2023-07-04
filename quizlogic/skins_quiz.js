a2c26f6e50cc58a0b69cb3d87ea98eff943ca951
christianhffm <your_donotreply_email_id>
2023-07-04 22:40:28 +0200

    sound button, added right sound

--- quizlogic/skins_quiz.js
+++ quizlogic/skins_quiz.js
@@ -167,6 +167,7 @@ function checkAnswer() {
       displayQuestion();
     }
     attemptedChampions = [];
+    playCorrectSound();
   } else {
     if (!attemptedChampions.includes(userAnswer)) { // Check if the champion guess is not already in the attempted list
       attemptedChampions.push(userAnswer);
@@ -178,6 +179,12 @@ function checkAnswer() {
   showFilteredChampions();
 }
 
+function playCorrectSound() {
+  const correctSound = document.getElementById('correctSound');
+  correctSound.play().catch(error => {
+    console.log('Error playing correct sound:', error);
+  });
+}
 
 function capitalizeChampionName(champion) {
   if (champion.toLowerCase() === "jarvan iv") {
