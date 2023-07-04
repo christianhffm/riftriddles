686e16a3db56f479b82a8d8ba967e426bd6db0e2
christianhffm <your_donotreply_email_id>
2023-07-04 23:05:27 +0200

    completed second mute button

--- templates/header.js
+++ templates/header.js
@@ -10,41 +10,66 @@ if (!headerPlaceholder.innerHTML) {
       // Set the fetched HTML as the content of the placeholder element
       headerPlaceholder.innerHTML = data;
 
-      // Once the content is inserted, you can access and modify the mute button element
-      const muteButton = document.getElementById('muteButton');
-      const muteImage = document.getElementById('muteImage');
+      // Once the content is inserted, you can access and modify the mute button elements
+      const muteMusicButton = document.getElementById('muteMusicButton');
+      const muteEffectsButton = document.getElementById('muteEffectsButton');
+      const muteMusicImage = document.getElementById('muteMusicImage');
+      const muteEffectsImage = document.getElementById('muteEffectsImage');
       const audioElements = document.getElementsByTagName('audio');
 
-      // Initialize the state and images for the button
-      let isMuted = false; // Set the initial state to muted
-      const unmuteImagePath = '../icons/header-icons/mute.png';
-      const muteImagePath = '../icons/header-icons/sound.png';
+      // Initialize the state and images for the buttons
+      let isMusicMuted = false; // Set the initial state for music to unmuted
+      let isEffectsMuted = false; // Set the initial state for effects to unmuted
+      const unmuteMusicImagePath = '../icons/header-icons/music.png';
+      const muteMusicImagePath = '../icons/header-icons/slash.png';
+      const unmuteEffectsImagePath = '../icons/header-icons/sound.png'; // Specify the actual path to unmute effects image
+      const muteEffectsImagePath = '../icons/header-icons/mute.png'; // Specify the actual path to mute effects image
 
-      // Set the initial image
-      muteImage.setAttribute('src', isMuted ? muteImagePath : unmuteImagePath);
+      // Set the initial images for the buttons
+      muteMusicImage.setAttribute('src', isMusicMuted ? muteMusicImagePath : unmuteMusicImagePath);
+      muteEffectsImage.setAttribute('src', isEffectsMuted ? muteEffectsImagePath : unmuteEffectsImagePath);
 
-      // Function to update the mute state and image
-      const updateMuteState = () => {
-        isMuted = !isMuted;
-        muteImage.setAttribute('src', isMuted ? muteImagePath : unmuteImagePath);
-        localStorage.setItem('muteState', isMuted.toString());
+      // Function to update the mute state and images for music
+      const updateMusicMuteState = () => {
+        isMusicMuted = !isMusicMuted;
+        muteMusicImage.setAttribute('src', isMusicMuted ? muteMusicImagePath : unmuteMusicImagePath);
+        localStorage.setItem('musicMuteState', isMusicMuted.toString());
 
-        // Mute or unmute all audio elements
+        // Mute or unmute music audio elements
         for (let i = 0; i < audioElements.length; i++) {
-          audioElements[i].muted = isMuted;
+          if (audioElements[i].classList.contains('music')) {
+            audioElements[i].muted = isMusicMuted;
 
-          // Play the audio element if it is not muted
-          if (!isMuted) {
-            audioElements[i].play().catch(error => {
-              console.log('Error playing audio:', error);
-            });
+            // Play the audio element if it is not muted
+            if (!isMusicMuted) {
+              audioElements[i].play().catch(error => {
+                console.log('Error playing audio:', error);
+              });
+            }
           }
         }
       };
 
-      // Set initial mute state and add click event listener
-      updateMuteState();
-      muteButton.addEventListener('click', updateMuteState);
+      // Function to update the mute state and images for effects
+      const updateEffectsMuteState = () => {
+        isEffectsMuted = !isEffectsMuted;
+        muteEffectsImage.setAttribute('src', isEffectsMuted ? muteEffectsImagePath : unmuteEffectsImagePath);
+        localStorage.setItem('effectsMuteState', isEffectsMuted.toString());
+      
+        // Mute or unmute effects audio elements
+        for (let i = 0; i < audioElements.length; i++) {
+          if (audioElements[i].classList.contains('effects')) {
+            audioElements[i].muted = isEffectsMuted;
+          }
+        }
+      };
+      
+
+      // Set initial mute state and add click event listeners to the buttons
+      updateMusicMuteState();
+      updateEffectsMuteState();
+      muteMusicButton.addEventListener('click', updateMusicMuteState);
+      muteEffectsButton.addEventListener('click', updateEffectsMuteState);
     })
     .catch(error => {
       console.log('Error fetching header.html:', error);
