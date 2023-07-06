3682ba82c9339b21433b2abe360fb39412edd873
christianhffm <your_donotreply_email_id>
2023-07-06 20:44:11 +0200

    just works, colors missing

--- quizlogic/champion_quiz.js
+++ quizlogic/champion_quiz.js
@@ -36,49 +36,74 @@ function selectRandomChampion() {
 }
 
 function check() {
-    const guess = answerElement.value.trim().toLowerCase();
-    const answer = currentChampionName.trim().toLowerCase();
+  const guess = answerElement.value.trim().toLowerCase();
+  const answer = currentChampionName.trim().toLowerCase();
 
-    console.log("checking ...");
+  console.log("checking ...");
 
-    if (guess === answer) {
-        console.log("correct");
-        currentCounter++;
-        if (currentCounter < championEntries.length) {
-            selectRandomChampion();
-        } else {
-            shuffleQuestions();
-            currentCounter = 0;
-            displayQuestion();
-        }
+  if (guess === answer) {
+    console.log("correct");
 
-        playCorrectSound();
+    const imageContainer = document.querySelector('.imagecontainer');
+    imageContainer.innerHTML = '';
 
+    currentCounter++;
+    if (currentCounter < championEntries.length) {
+      selectRandomChampion();
     } else {
-        console.log("wrong");
-
-        if (!attemptedChampions.includes(guess)) { // Check if the champion guess is not already in the attempted list
-            attemptedChampions.push(guess);
-            attemptedChampions = [];
-        }
+      shuffleQuestions();
+      currentCounter = 0;
+      displayQuestion();
     }
-    answerElement.value = '';
-    showFilteredChampions();
+
+    playCorrectSound();
+
+  } else {
+    console.log("wrong");
+
+    const imageContainer = document.querySelector('.imagecontainer');
+
+    // Create a new row element
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    const guessedChampionEntry = championEntries.find(champion => champion.name.toLowerCase() === guess);
+
+    if (guessedChampionEntry) {
+      // Create and append the champion data boxes
+      for (const category of Object.keys(guessedChampionEntry)) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        box.textContent = guessedChampionEntry[category];
+        row.appendChild(box);
+      }
+    }
+
+    // Insert the new row at the beginning of the image container (reversed order)
+    imageContainer.insertBefore(row, imageContainer.firstChild);
+
+    if (!attemptedChampions.includes(guess)) { // Check if the champion guess is not already in the attempted list
+      attemptedChampions.push(guess);
+    }
+  }
+  answerElement.value = '';
+  showFilteredChampions();
 }
+
 
 answerElement.addEventListener('keydown', (event) => {
     const filteredChampions = championListElement.getElementsByTagName('li');
     const selectedChampion = filteredChampions[selectedChampionIndex]?.textContent;
-  
+
     if (event.key === 'Enter') {
-      if (selectedChampion) {
-        answerElement.value = selectedChampion;
-      } else if (filteredChampions.length > 0) {
-        answerElement.value = filteredChampions[0].textContent;
-      }
-  
-      check();
+        if (selectedChampion) {
+            answerElement.value = selectedChampion;
+        } else if (filteredChampions.length > 0) {
+            answerElement.value = filteredChampions[0].textContent;
+        }
+
+        check();
     }
-  });
+});
 
 initializeQuestions();
